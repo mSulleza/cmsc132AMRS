bf5fa7fa3467b7c8928a0ffe2f5e8425ccd9dc74
mSulleza <marksulleza@gmail.com>
2017-04-02 13:38:29 +0800

    added a parser function

---
+++ Main.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+	static LinkedList<String> instruction = new LinkedList<String>();
+	static LinkedList<String> registers = new LinkedList<String>();
+
+	public static void parser(String line)
+	{
+		String[] temp = line.split(" ", 2); // splits the instruction from the register (2 ensures that it only gets the first occurence of the space character)
+		instruction.add(temp[0]); // stores the instruction
+		String[] reg = temp[1].replace(" ", "").split(","); // splits the registers separated by comma
+		
+		for (int i = 0; i < reg.length; i++)
+		{
+			registers.add(reg[i]);
+		}
+	}
+	public static void loadFile(String path)
+	{
+		try
+		{
+			FileReader fr = new FileReader(path);
+			BufferedReader br = new BufferedReader(fr);
+
+			String line;
+
+			while ((line = br.readLine()) != null)
+			{
+				parser(line);
+			}
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		
+	}
+	public static void main(String[] args)
+	{
+		loadFile("input.txt");
+	}
+}
